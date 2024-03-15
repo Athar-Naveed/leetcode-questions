d6955557a05d65ef80373da53b673b71e4863673
Athar Naveed <94740625+Athar-Naveed@users.noreply.github.com>
2024-03-15 14:46:18 +0500

    Time: 804 ms (13.89%) | Memory: 27.6 MB (13.34%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, array: List[int]) -> int:
+        if len(array) == 1:
+            return 0
+        else:
+            profit_keeper = {}
+            maxi,mini = 0,0
+            for i in range(len(array)-1):
+                if array[i+1] > array[mini]:
+                    maxi = i+1
+                elif array[i+1] < array[maxi]:
+                    mini = i+1
+                if mini < maxi:
+                    profit_keeper.update({array[maxi]-array[mini]:(mini,maxi)})
+            if profit_keeper:
+                return max(p for p in profit_keeper.keys())
+            else:
+                return 0
+                
